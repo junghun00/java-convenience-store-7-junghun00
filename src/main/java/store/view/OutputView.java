330ec518c54ba6d1d624740ae4f09d98d50a8504
junghun00 <ji8657447@gmail.com>
2024-11-11 10:25:45 +0900

    fix: 출력 양식 수정

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -10,17 +10,17 @@ import store.model.Store;
 public class OutputView {
     private static final String ERROR_ANSWER = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
 
-    private final String WECOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n";
+    private final String WECOME_MESSAGE = "\n안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n";
     private final String RESULT_START_MESSAGE = "==============W 편의점================";
     private final String RESULT_MESSAGE = "%-16s\t%-5s\t%s\n";
-    private final String ORDER_LIST = "%-16s\t%-7d\t%d\n";
+    private final String ORDER_LIST = "%-16s\t%-7d\t%,d\n";
     private final String PROMOTION_MESSAGE = "=============증     정===============";
-    private final String PROMOTION_LIST = "%-16s\t%-5d\n";
+    private final String PROMOTION_LIST = "%-16s\t%,-5d\n";
     private final String LINE = "====================================";
 
 
     public void printError(String errorMessage) {
-        System.out.print(ERROR + errorMessage + RETRY);
+        System.out.println(ERROR + errorMessage + RETRY);
     }
 
     public static void printErrorAnswer() {
@@ -78,9 +78,9 @@ public class OutputView {
         for (Receipt receipt : receipts) {
             totalQuantity += receipt.getQuantity();
         }
-        System.out.printf("%-16s\t%-7d\t%-7d\n", "총구매액", totalQuantity, totalOrder);
-        System.out.printf("%-23s\t%d\n", "행사할인", totalPromotion);
-        System.out.printf("%-23s\t%d\n", "멤버십할인", discount);
-        System.out.printf("%-23s\t%d\n", "내실돈", (totalOrder - totalPromotion - discount));
+        System.out.printf("%-16s\t%-7d\t%,-7d\n", "총구매액", totalQuantity, totalOrder);
+        System.out.printf("%-23s\t%,d\n", "행사할인", totalPromotion);
+        System.out.printf("%-23s\t%,d\n", "멤버십할인", discount);
+        System.out.printf("%-23s\t%,d\n", "내실돈", (totalOrder - totalPromotion - discount));
     }
 }
